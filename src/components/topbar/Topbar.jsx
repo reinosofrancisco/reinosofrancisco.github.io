4297f8ab36c7d38d023f67a3ac2cb76ccdcf9f82
reinosofrancsico <reinosofrancisco_@hotmail.com>
2022-02-01 22:43:08 -0300

    Source Code Changes

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,9 +4,9 @@ import {Person, Mail, GitHub} from "@material-ui/icons"
 export default function Topbar({menuOpen, setMenuOpen}) {
   return <div className={"topbar " + (menuOpen && "active")}>
     <div className="wrapper">
-      <div className="left">
-        
+
         <a href="#intro" className="logo">Home.</a>
+        
         <a href="https://github.com/reinosofrancisco" rel="noopener noreferrer" target="_blank" className="logo"><GitHub className="logo"/></a>
         
         {/* npm add @material-ui/core 
@@ -17,20 +17,17 @@ export default function Topbar({menuOpen, setMenuOpen}) {
           <Person className="icon"/>
           <span> +54 9 221 420 6558 </span>
         </div>
+
         <div className="itemContainer">
           <Mail className="icon"/>
           <span> reinosofrancisco99@gmail.com</span>
         </div>
-      </div>
 
-      <div className="right">
         <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
           <span className="line1"></span>
           <span className="line2"></span>
           <span className="line3"></span>
-
         </div>
-      </div>
 
     </div>
   </div>;
